32ae020703b20734933fa5277881778927c64f49
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-10-24 15:11:19 +0700

    update

--- project/src/views/admin/App.js
+++ project/src/views/admin/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Navigation from './Navigation.js';
 function App() {
   return (
-    <>
-    
-    </>
+    <div class="wrapper">
+      <Navigation />
+    </div>
   );
 }
 
--- project/src/views/admin/Navigation.js
+++ project/src/views/admin/Navigation.js
@@ -1,7 +1,50 @@
 import React from "react";
+import "../../styles/admin.css";
 class Navigation extends React.Component {
     render() {
-        
+        return (
+            <>
+                <nav id="sidebar" class="sidebar js-sidebar">
+                    <div class="sidebar-content js-simplebar">
+                        <a class="sidebar-brand" href="#">
+                            <span class="align-middle">Admin</span>
+                        </a>
+
+                        <ul class="sidebar-nav">
+                            <li class="sidebar-header">
+                                Pages
+                            </li>
+
+                            <li class="sidebar-item active">
+                                <a class="sidebar-link" href="#">
+                                    <span class="align-middle">Quản lí chủ trọ</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a class="sidebar-link" href="#">
+                                    <span class="align-middle">Quản lí gói đăng ký</span>
+                                </a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a class="sidebar-link" href="#">
+                                    <span class="align-middle">Quản lí yêu cầu</span>
+                                </a>
+                            </li>
+                            <a class="sidebar-link" href="#">
+                                <span class="align-middle">Quản lí hình ảnh</span>
+                            </a>
+                            <li class="sidebar-item">
+                                <a class="sidebar-link" href="#">
+                                    <span class="align-middle">Thông tin tài khoản</span>
+                                </a>
+                            </li>
+                        </ul>
+
+
+                    </div>
+                </nav>
+            </>
+        )
     }
 }
 export default Navigation;
