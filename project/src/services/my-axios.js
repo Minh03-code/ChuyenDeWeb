d55777e7df19181c139b023107b7623bc6a68166
phuc0707 <nguyenhphuc0707@gmail.com>
2023-10-27 15:20:51 +0700

    Updata Call api getAllBanner

--- project/src/services/my-axios.js
+++ project/src/services/my-axios.js
@@ -1,16 +1,21 @@
 import axios from "axios";
+
+const baseURL = "http://127.0.0.1:8000/";
 const myAxios = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
-  timeout: 1000*60,
+  baseURL: baseURL,
+  timeout: 1000 * 60,
 });
 // Add a response interceptor
-myAxios.interceptors.response.use(function (response) {
-  // Any status code that lie within the range of 2xx cause this function to trigger
-  // Do something with response data
-  return response.data;
-}, function (error) {
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
-  return Promise.reject(error);
-});
-export default myAxios;
+myAxios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+export { myAxios, baseURL };
