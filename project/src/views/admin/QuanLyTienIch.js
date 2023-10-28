f00773040cf163f1de1602e9b3dd2af68275b577
Dung1610 <nguyentand1610@gmail.com>
2023-10-28 13:21:36 +0700

    danh sach tien ich

--- project/src/views/admin/QuanLyTienIch.js
+++ project/src/views/admin/QuanLyTienIch.js
@@ -1,6 +1,21 @@
+import axios from 'axios';
 import React from 'react';
 class QuanLyTienIch extends React.Component {
+    state = {
+        listTienIch: []
+    }
+    async componentDidMount() {
+        let res = await axios.get('http://127.0.0.1:8000/api/laytatcatienich');
+        if (res.data != null) {
+            this.setState({
+                listTienIch: res.data
+            })
+            console.log(this.state.listTienIch);
+            console.log(res);
+        }
+    }
     render() {
+        let { listTienIch } = this.state;
         return (
             <>
                 <div className="main">
@@ -28,7 +43,7 @@ class QuanLyTienIch extends React.Component {
                                             <th className="d-none d-md-table-cell">Chức năng</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
+                                    {/* <tbody>
                                         <tr>
                                             <td>#1111</td>
                                             <td className="d-none d-xl-table-cell">11561665415-banner.png</td>
@@ -37,6 +52,20 @@ class QuanLyTienIch extends React.Component {
                                                 <a href="#" className="btn btn-primary">Chỉnh sửa</a>
                                             </td>
                                         </tr>
+                                    </tbody> */}
+                                    <tbody>
+                                        {listTienIch && listTienIch.length > 0 && listTienIch.map((item, index) => {
+                                            return (
+                                                <tr>
+                                                    <td>{item.id}</td>
+                                                    <td className="d-none d-xl-table-cell">{item.ten}</td>
+                                                    <td className="d-none d-xl-table-cell">{item.hinh}</td>
+                                                    <td className="d-none d-md-table-cell">
+                                                        <a href="" className="btn btn-primary">Chỉnh sửa</a>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
                                     </tbody>
                                 </table>
                             </div>
