7a8bb210ca7ff39a86008205ae7fa268065cd8fb
Dung1610 <nguyentand1610@gmail.com>
2023-11-17 13:17:23 +0700

    fix

--- project/src/views/chutro/GoiDangDung.js
+++ project/src/views/chutro/GoiDangDung.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import chitietanh from "../../images/chitietphongtro.jpg"
+import chitietanh from "../../images/avt1.jpg"
 class GoiDangDung extends React.Component {
-    render() {
-        return (
-            <>
-             <div class="page-heading header-text">
+  render() {
+    return (
+      <>
+        <div class="page-heading header-text">
           <div class="container">
             <div class="row">
               <div class="col-lg-12">
@@ -15,41 +15,41 @@ class GoiDangDung extends React.Component {
           </div>
         </div>
         <div class="single-product section">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="left-image">
-            <img src={chitietanh} alt=""/>
-          </div>
-        </div>
-        <div class="col-lg-6 align-self-center">
-          <h4>Gói Cho Thuê 10 Phòng 1 Tháng</h4>
-        
-         
-          <div className='chitietgoidangki'>
-            <h2 className='ten_chu_tro'>Nguyễn Gia Nghiêm</h2>
-            <div className='chutro_info'><b>Trạng Thái: </b> Chưa Xác Thực</div>
-            <div className='chutro_info'><b>Số Điện Thoại: </b>C111111111111</div>
-            <div className='chutro_info'><b>Số Tài Khoản:</b> 1111111111111</div>
-            <div className='chutro_info'><b>Số Tài Khoản Ngân Hàng:</b> 1111111111111</div>
-            <div className='chutro_info'><b>Tên Người Thụ Hưởng:</b> NGUYEN GIA NGHIEM</div>
-            <button className='btn btn-primary bbt'>Chỉnh Sửa</button>
-            <button className='btn btn-warning bbt'>Đổi Mật Khẩu</button>
-            <button className='btn btn-success bbt'>Xác Thực</button>
-            <button className='btn btn-danger bbt'>Đăng Xuất</button>
-         
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="left-image">
+                  <img src={chitietanh} alt="" />
+                </div>
+              </div>
+              <div class="col-lg-6 align-self-center">
+                <h4>Gói Cho Thuê 10 Phòng 1 Tháng</h4>
 
+
+                <div className='chitietgoidangki'>
+                  <h2 className='ten_chu_tro'>Nguyễn Gia Nghiêm</h2>
+                  <div className='chutro_info'><b>Trạng Thái: </b> Chưa Xác Thực</div>
+                  <div className='chutro_info'><b>Số Điện Thoại: </b>C111111111111</div>
+                  <div className='chutro_info'><b>Số Tài Khoản:</b> 1111111111111</div>
+                  <div className='chutro_info'><b>Số Tài Khoản Ngân Hàng:</b> 1111111111111</div>
+                  <div className='chutro_info'><b>Tên Người Thụ Hưởng:</b> NGUYEN GIA NGHIEM</div>
+                  <button className='btn btn-primary bbt'>Chỉnh Sửa</button>
+                  <button className='btn btn-warning bbt'>Đổi Mật Khẩu</button>
+                  <button className='btn btn-success bbt'>Xác Thực</button>
+                  <button className='btn btn-danger bbt'>Đăng Xuất</button>
+
+
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="sep"></div>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="col-lg-12">
-          <div class="sep"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-    
-                 </>
-        )
-    }
+
+      </>
+    )
+  }
 }
 export default GoiDangDung;
