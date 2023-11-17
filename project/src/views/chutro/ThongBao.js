7bcb2c6a9b8ed75c1961b236598589a05790706d
kietdo9488 <kietdo9488@gmail.com>
2023-11-17 15:50:26 +0700

    update giao dien thong bao

--- project/src/views/chutro/ThongBao.js
+++ project/src/views/chutro/ThongBao.js
@@ -40,7 +40,7 @@ class ThongBao extends React.Component {
                                     <div>
                                         {listThongBaoById && listThongBaoById.length > 0 && listThongBaoById.map((item, index) => {
                                             return (
-                                                <div class="col-lg-12 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
+                                                <div class="col-lg-12 col-md-6 align-self-center mb-30 trending-items col-md-6">
                                                     <div class="item">
                                                         <div class="down-content">
                                                             <span class="category">{item.noiDung}</span>
@@ -58,13 +58,13 @@ class ThongBao extends React.Component {
                             }
                             {/* <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 str">
                                 <div class="item">
-                                    
+
                                     <div class="down-content">
-                                    <span class="category">Thanh toán thành...</span>
+                                        <span class="category">Thanh toán thành...</span>
                                         <h4>Admin</h4>
                                         <a href="product-details.html"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
-  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-</svg></a>
+                                            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                                        </svg></a>
                                     </div>
                                 </div>
                             </div> */}
