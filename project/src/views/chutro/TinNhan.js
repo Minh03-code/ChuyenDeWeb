4c8eb02915589df426c0a78ace537ada2cd4e1cd
gianghiem0310 <jangiim93nt@gmail.com>
2023-11-18 11:43:08 +0700

    update dulieu

--- project/src/views/chutro/TinNhan.js
+++ project/src/views/chutro/TinNhan.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import avt from "../../styles/chutro/library/images/avatars/4.jpg";
 import { getProfileChuTro,getListTinNhan } from '../../services/admin/NghiemService';
 import { baseURL } from '../../services/my-axios.js';
 class TinNhan extends React.Component {
@@ -82,26 +81,45 @@ class TinNhan extends React.Component {
                                 
 
                                 </div>
-                                <div className='col-8'>
-
-                                </div>
+                                
                                         </>:<>
                                         <div className='col-4 list_nguoi_nhan_tin'>
                                     <div className='tieude_tinNhan'>
                                     <h2>Chats</h2>
                                     </div>
                                     <div className="card_nhan_tin">
-                                    
+                                        <h3>
                                         Chưa Có Dữ Liệu
+                                        </h3>
+                                      
 
                                     </div>
 
                                 </div>
-                                <div className='col-8'>
-
-                                </div>
+                                
                                         </>
                                    }
+                                   {/* Hiển thị tin nhắn */}
+                                   <div className='col-8 hien_thi_tin_nhan'>
+                                        <div className='tieu_de_ten_nguoi_nhan'>
+                                        <div className='img_doi_phuong'>
+                                                <img className='src_avt_doi_phuong' src={baseURL+chuTro.hinh}/>
+                                        </div>
+                                        <div className='ten_doi_phuong'>
+                                                {chuTro.ten}
+                                        </div>
+                                        </div>
+                                        <div className='vung_gui_tin_nhan'>
+                                            <textarea type="text" className='input_tin_nhan' rows={1} placeholder='Nhập tin nhắn...'/>
+
+                                            <button className='btn_send'>
+
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+                                            </svg>
+                                            </button>
+                                        </div>
+                                    </div>
                             </div>
 
                         </div>
