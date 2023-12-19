56cdaccf9c98a6dd396f8e03d1cf92034355080b
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-12-19 14:57:49 +0700

    update banner auto slide and export component ImagesBanner

--- project/src/views/item/HeaderNguoiThue.js
+++ project/src/views/item/HeaderNguoiThue.js
@@ -1,64 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import ImagesBanner from './ImagesBanner';
 import imgGhep from "./imgs/icon_ghep.png";
 import imgRandom from "./imgs/icon_random.png";
 import imgRecomment from "./imgs/icon_recomment.png";
 import imgVDRoom from "./imgs/img2.jpg";
-import { getAllBannerApi } from '../../services/admin/PhucService';
-import { baseURL } from '../../services/my-axios';
+
 export default function HeaderNguoiThue(props) {
     const navigation = useNavigate();
-    const[listBanner,setListBanner]= useState([]);
-    useEffect(() => {
-         fetchData();
-    }, []);
-    const fetchData = async()=>{
-        let res = await getAllBannerApi();
-        if(res!=null){
-         setListBanner(res);
-        }
-    }
+    
+    
     const chuyenManHinhGoiY = ()=>{
         navigation("/nguoithue/danhsachphonggoiy")
     }
-    const scrollAutoImages = () => {
-        let quantity = 1;
-        const imgContainer = document.querySelector('.image-container');
-        const listItemImages = document.querySelectorAll('.item-img');
-        const itemImage = document.querySelector('.item-img');
-        setInterval(() => {
-            if (quantity < listItemImages.length) {
-                listItemImages.forEach(element => {
-                    element.style.transform = `translateX(-${100 * quantity}%)`;
-                });
-
-            }
-            else {
-                quantity = 0;
-                listItemImages.forEach(element => {
-                    element.style.transform = `translateX(${100 * quantity}%)`;
-                });
-                
-                
-            }
-            quantity++;
-        }, 2000);
-    }
-    scrollAutoImages();
+    
     return (
         <>
             <header>
-                <div className="image-container">
-                    {
-                        listBanner&&listBanner.length>0?listBanner.map((item,index)=>{
-                            return(
-                                <img className="the_img_banner item-img" src={baseURL+item.hinhBanner} alt={baseURL+item.hinhBanner} key={index}/>
-                            )
-                        })
-                        :<>
-                        </>
-                    }
-                </div>
+                <ImagesBanner/>
                 <div className="container-search">
                     <div className="search">
                         <div className="search-form">
